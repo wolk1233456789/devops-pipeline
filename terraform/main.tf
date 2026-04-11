2070525536bfb50b1b481e4f3b42ffca22ebba77
keylita <key2005que.z@gmail.com>
2026-04-11 14:42:33 -0500

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "vm" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  key_name = "aws-key-devops"
+
+  tags = {
+    Name = "Jenkins-VM"
+  }
+}
