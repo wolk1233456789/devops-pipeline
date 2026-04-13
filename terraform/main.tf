904c720aca375566b5f45f81032d667ab5a35861
keylita <key2005que.z@gmail.com>
2026-04-12 22:08:52 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -5,10 +6,15 @@ provider "aws" {
 resource "aws_instance" "vm" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t3.micro"
-
-  key_name = "aws-key-devops"
+  key_name      = "aws-key-devops"
 
   tags = {
     Name = "Jenkins-VM"
   }
+}
+
+# ESTA ES LA PARTE QUE FALTA Y ES CRÍTICA
+output "public_ip" {
+  description = "IP pública de la instancia creada"
+  value       = aws_instance.vm.public_ip
 }
