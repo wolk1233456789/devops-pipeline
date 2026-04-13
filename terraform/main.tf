715f1beb3fb6f59433910e88535397129b510d8c
keylita <key2005que.z@gmail.com>
2026-04-12 19:05:12 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "vm" {
   ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   key_name = "aws-key-devops"
 
