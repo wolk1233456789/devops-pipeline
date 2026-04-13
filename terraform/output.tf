69950ec23517fc2f6c2a24105b38b4038159adec
keylita <key2005que.z@gmail.com>
2026-04-12 22:10:00 -0500

    Configure AWS provider and create EC2 instance
    Add AWS provider and EC2 instance configuration.

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "vm" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2
+  instance_type = "t3.micro"
+  key_name      = "aws-key-devops"
+
+  tags = {
+    Name = "Jenkins-VM"
+  }
+}
+
 output "public_ip" {
   value = aws_instance.vm.public_ip
 }
